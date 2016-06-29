3ad7e3cc5d779d3edebdbaf4ef707ca486f4353d
Victor de Souza <victorsouza.nave@gmail.com>
2016-06-29 11:10:36 -0300

    Primeiro commit

---
+++ Exercicios/Ex01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+
+    /// <summary>
+    /// Fazer um programa que leia três valores numéricos, 
+    /// determine e imprima o menor deles.
+    /// </summary>
+
+    public class Ex01
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Calcular (int a, int b, int c)
+        {
+            int menor = a;
+
+            if (b < menor)
+                menor = b;
+            if (c < menor)
+                menor = c;
+
+            return menor;
+
+
+        }
+
+    }
+}
---
+++ ExerciciosTest/Ex01Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercicios;
+
+namespace ExerciciosTest
+{
+    [TestClass]
+    public class Ex01Test
+    {
+        [TestMethod]
+        public void Primeiro_Numero_For_Menor()
+        {
+            Ex01 ex = new Ex01();
+
+            int resultado = ex.Calcular (1, 2, 3);
+
+            Assert.AreEqual(1, resultado);
+
+        }
+
+        [TestMethod]
+        public void Segundo_Numero_For_Menor()
+        {
+            Ex01 ex = new Ex01();
+
+            int resultado = ex.Calcular(2, 1, 3);
+
+            Assert.AreEqual(1, resultado);
+
+        }
+
+        [TestMethod]
+        public void Terceiro_Numero_For_Menor()
+        {
+            Ex01 ex = new Ex01();
+
+            int resultado = ex.Calcular(2, 3, 1);
+
+            Assert.AreEqual(1, resultado);
+
+        }
+    }
+}
